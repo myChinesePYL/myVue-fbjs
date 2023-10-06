67a359a8cb24eb46e6ea1219ed8570422159b5d6
myChinesePYL <3223285598@qq.com>
2023-10-06 10:59:23 +0800

    添加了触摸屏适配

--- src/components/drag.ts
+++ src/components/drag.ts
@@ -3,33 +3,30 @@ import $ from 'jquery'
 export default class main {
   public target: HTMLElement
   public callback: Function
-  public args: any = {}
+  public args: any = {
+    x: 0,
+    y: 0
+  }
   public back: any = {}
 
   constructor(eventElement: HTMLElement, callback: Function) {
     this.target = eventElement
     this.callback = callback
 
-    $(this.target).on('pointermove', (e: JQuery.Event) => {
-      if (e.buttons !== 1 || e.clientX == undefined || e.clientY == undefined) {
-        this.back.x = e.clientX
-        this.back.y = e.clientY
+    $(this.target).on('pointermove', (e: JQuery.Event) => { // 移动检测
+      if (e.buttons !== 1 || e.screenX == undefined || e.screenY == undefined) {
+        this.back.x = e.screenX
+        this.back.y = e.screenY
         return
       }
-      if (this.args.x === 0 && this.args.y === 0) {
-        this.back.x = e.clientX
-        this.back.y = e.clientY
-        return
-      }
-      if (this.back.x == undefined) this.back.x = e.clientX
-      if (this.back.y == undefined) this.back.y = e.clientY
+      if (this.back.x == undefined || !(e.screenX - this.back.x >= -30 && e.screenX - this.back.x <= 30)) this.back.x = e.screenX
+      if (this.back.y == undefined || !(e.screenY - this.back.y >= -30 && e.screenY - this.back.y <= 30)) this.back.y = e.screenY
 
-      this.args.x = e.clientX - this.back.x
-      this.args.y = e.clientY - this.back.y
+      this.args.x = e.screenX - this.back.x
+      this.args.y = e.screenY - this.back.y
 
-      this.back.x = e.clientX
-      this.back.y = e.clientY
-
+      this.back.x = e.screenX
+      this.back.y = e.screenY
       this.callback(this.args)
     })
 
